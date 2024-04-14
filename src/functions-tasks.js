329cd981d6454866629ec9272d301500497ff29b
Konstantin Kukushkin <k.k.o.lukoml@mail.ru>
2024-04-14 23:27:55 +0300

    init: add second solutions

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -169,8 +169,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
@@ -190,8 +192,14 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let nextId = startFrom;
+
+  return () => {
+    const currentId = nextId;
+    nextId += 1;
+    return currentId;
+  };
 }
 
 module.exports = {
